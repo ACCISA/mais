5fbf8ae8ca65c3b249cdde79698f3e7c6cabcafd
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-09-30 17:21:29 -0400

    houses ui

--- client/src/components/dashboard/House.jsx
+++ client/src/components/dashboard/House.jsx
@@ -1,25 +1,17 @@
 import { Link, useParams } from "react-router-dom";
-import houseImg from "../../images/house.jpg";
+import '../../styles.css';
 
-export default function House() {
+export default function House({ owner }) {
   return (
     <>
-      <h1>House Name</h1>
-      <p>Address</p>
-      <p>PRICE </p>
-      <div className="mt-4">
-        <Link
-          to={"/houses/" + "temp"}
-          className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
-        >
-          <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
-            <img className="rounded-2xl w-full object-cover " src={houseImg} />
-          </div>
-          <div className="grow-0 shrink">
-            <h2 className="text-xl">Title</h2>
-            <p className="text-sm mt-2">Description</p>
-          </div>
-        </Link>
+      <div className="pet">
+        <h2>{owner.name}</h2>
+        <img src={houseImg} alt="Picture of house" />
+        <p>Address: {owner.address}</p>
+        <p>Price: {owner.price}</p>
+        <p>Owners Name: {owner.ownerName}</p>
+        <p>Owners Email: {owner.ownerEmail}</p>
+        <button>Interested</button>
       </div>
     </>
   );
